29b3434bae24e96bfdf006ab50b015a85f135d0e
Sushant <sushantraj62060@gmail.com>
2026-09-05 23:06:32 +0530

    Array Basic in Java

---
+++ src/03_Arrays/01_Declaration.java
@@ -0,0 +1,27 @@
+class Declaration{
+    public static void main(String[] args){
+        //Lets we have some int values to store.
+        int a1= 10;
+        int a2 = 20;
+        int a3 = 30;
+        //so much time taking and complex. also very hard to manage for big projects.
+
+        //Here the concept comes into the game
+        int num[] = {10, 20, 30};
+
+
+
+        //*Note: Once you define the size of an array, you have to be fixed with it.
+
+        //!What if you don't know the values
+        int num1[] = new int[4];
+
+        //also the emplty array initializes by "0" at all the places of array.
+
+
+
+
+        //!To change the format of efk
+
+    }
+}
