e34cb00b4ead2009f662c393d521015ec0a9d32d
Sushant <sushantraj62060@gmail.com>
2026-09-19 23:00:53 +0530

    Add a basic concept of java

---
+++ src/01_Basics/11_NamingConvention.java
@@ -0,0 +1,17 @@
+/*
+class and interfaces --> Calc, Human, ShowMarks, etc --> CamelCase  (First letter capital and other small)
+variables and methods --> marks, show() --> (all small letters)
+constants --> PIE, CONSTANT, BRAND ---> all capital
+*/
+
+
+
+/*
+Camel Casing
+ex - showMyMarks()-> (method), MarksManagement-> (class), Human() -> (constructor), human -> (variable)  etc 
+*/
+
+/*
+Snake Casing
+ex - show_my_marks(), etc
+*/
